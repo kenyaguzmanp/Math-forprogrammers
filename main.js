9971435cf5d16784bc07df93615bd25d2dd112b1
renyalvarado <renyalvarado@gmail.com>
2018-12-09 19:11:21 -0300

    Add text in origin and axis

--- main.js
+++ main.js
@@ -25,13 +25,39 @@ function drawRectangle(context, x, y, width, height, color) {
   context.fillRect(x,y, width, height);
 }
 
+function drawTriangle(context, initX, initY, finalX, finalY, width, height) {
+  drawLine(context, initX, initY, finalX, finalY);
+}
+
+function drawText(context, text, x, y) {
+  context.font = '12px serif';
+  context.fillText(text, x, y);
+}
+
+function drawOrigin(context, x, y, width, color) {
+  drawRectangle(context, x - (width / 2), y - (width / 2), 5, width, color);
+  drawText(context, "Origin", x + 10, y - 10);
+}
+
+function drawXAxis(context, width, height, margin) {
+  drawLine(context, margin, height / 2, width - margin, height / 2);                                                                                                                                                                                                                                                                                 
+  drawText(context, "X", width - margin, height / 2 - 10);
+  drawText(context, "-X", margin, height / 2 - 10);
+}
+
+function drawYAxis(context, width, height, margin){
+  drawLine(context, width / 2, margin, width / 2, height - margin);
+  drawText(context, "Y", width / 2 + 10, margin);
+  drawText(context, "-Y", width / 2 + 10, height - margin);
+}
+
 function drawCoordinateSystem(context, width, height, margin) {
-   // X axis                                                                                                                                                                                                                                                                                 
-   drawLine(context, width / 2, margin, width / 2, height - margin);
-   // Y axis
-   drawLine(context, margin, height / 2, width - margin, height / 2);
-   // Origin
-   drawRectangle(context, width / 2 - 2.5, height / 2 - 2.5, 5, 5, "blue");
+  // X axis
+  drawXAxis(context, width, height, margin);
+  // Y axis
+  drawYAxis(context, width, height, margin);
+  // Origin
+  drawOrigin(context, width / 2, height / 2, 5, "blue");
 }
 
 draw();
