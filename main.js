722139058b46a41401632d54e0affbf22a9ea110
renyalvarado <renyalvarado@gmail.com>
2018-12-10 00:09:26 -0300

    Add xGrid and yGrid

--- main.js
+++ main.js
@@ -5,28 +5,24 @@ function draw() {
   const marginVertical = 20;
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
-
-   drawCoordinateSystem(ctx, canvasWidth, canvasHeight, marginVertical);
+    drawCoordinateSystem(ctx, canvasWidth, canvasHeight, marginVertical);
 
   }
 }
 
 
-function drawLine(context, initX, initY, finalX, finalY) {
+function drawLine(context, initX, initY, finalX, finalY, color) {
   context.beginPath();       // Start a new path
   context.moveTo(initX, initY,);
   context.lineTo(finalX, finalY);
   context.lineWidth = 1;
+  context.strokeStyle=color;
   context.stroke();          // Render the path 
 }
 
 function drawRectangle(context, x, y, width, height, color) {
   context.fillStyle=color;
   context.fillRect(x,y, width, height);
-}
-
-function drawTriangle(context, initX, initY, finalX, finalY, width, height) {
-  drawLine(context, initX, initY, finalX, finalY);
 }
 
 function drawText(context, text, x, y) {
@@ -40,15 +36,43 @@ function drawOrigin(context, x, y, width, color) {
 }
 
 function drawXAxis(context, width, height, margin) {
-  drawLine(context, margin, height / 2, width - margin, height / 2);                                                                                                                                                                                                                                                                                 
+  drawLine(context, margin, height / 2, width - margin, height / 2, "black");                                                                                                                                                                                                                                                                                 
   drawText(context, "X", width - margin, height / 2 - 10);
   drawText(context, "-X", margin, height / 2 - 10);
 }
 
 function drawYAxis(context, width, height, margin){
-  drawLine(context, width / 2, margin, width / 2, height - margin);
+  drawLine(context, width / 2, margin, width / 2, height - margin, "black");
   drawText(context, "Y", width / 2 + 10, margin);
   drawText(context, "-Y", width / 2 + 10, height - margin);
+}
+
+function drawXGrid(context, initX, initY, finalX, finalY, numberOfElems, size = 5) {
+  const n = Math.round(numberOfElems);
+  const partitionX = Math.abs(finalX - initX) / n;
+  let acumX = initX;
+  for(i= 0; i <= n + 1; i++) {
+    drawLine(context, acumX, initY - size, acumX, initY + size, "red");
+    acumX = initX + i * partitionX;
+  }
+}
+
+function drawYGrid(context, initX, initY, finalX, finalY, numberOfElems, size = 5) {
+  const n = Math.round(numberOfElems);
+  const partitionY = Math.abs(finalY - initY) / n;
+  let acumY = initY;
+  for(j= 0; j <= n + 1; j++) {
+    drawLine(context, initX - size, acumY, initX + size, acumY, "red");
+    acumY = initY + j * partitionY;
+  }
+}
+
+function drawGrid(context, width, height, margin, numberOfElems, size) {
+  drawXGrid(context, margin, height / 2, width / 2, margin, numberOfElems, size);
+  drawYGrid(context, width / 2, margin, width / 2, height / 2, numberOfElems, size);
+
+  drawXGrid(context, width / 2, height / 2, width - margin, height - margin, numberOfElems, size);
+  drawYGrid(context, width / 2, height / 2, width / 2, height - margin, numberOfElems, size);
 }
 
 function drawCoordinateSystem(context, width, height, margin) {
@@ -58,6 +82,8 @@ function drawCoordinateSystem(context, width, height, margin) {
   drawYAxis(context, width, height, margin);
   // Origin
   drawOrigin(context, width / 2, height / 2, 5, "blue");
+  // Grid
+  drawGrid(context, width, height, margin, 8);
 }
 
 draw();
